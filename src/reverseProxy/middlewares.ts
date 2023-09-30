7f6c47c4b32602e243a110a976433e3890cb8862
GR__n <greens4acc@gmail.com>
2023-09-30 04:04:40 +0200

    idk

--- src/reverseProxy/middlewares.ts
+++ src/reverseProxy/middlewares.ts
@@ -4,7 +4,7 @@ const rateLimit = new Map();
 
 function corsMiddleware(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Contropl-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "*");
     next();
 };
